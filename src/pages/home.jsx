4bbbddf349301b1149abab0558ccb5db969ba40f
JosephDuvvani <josephduvvani@gmail.com>
2025-04-03 22:59:25 +0200

    Fetch all posts from the database
    Refresh token if access token has expired

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,12 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { AuthContext } from "../App";
 import Header from "../components/header";
+import { format } from "date-fns";
+import Cookies from "universal-cookie";
+import { fetchToken } from "../utils/utils";
+import { jwtDecode } from "jwt-decode";
 
 
 const Wrapper = styled.div`
@@ -47,6 +51,50 @@ const Login = styled(Button)`
 
 const Home = () => {
   const {user, setUser, isLoadingUser} = useContext(AuthContext);
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const url = "http://localhost:3000/posts/admin";
+      const cookies = new Cookies(null, {path: '/'});
+      let accessToken = cookies.get('jwt-access-blog-p');
+      const refreshToken = cookies.get('jwt-refresh-blog-p');
+      
+      if (!accessToken && refreshToken) {
+        const tokenUrl = 'http://localhost:3000/auth/token'
+        const data = await fetchToken(refreshToken, tokenUrl);
+        
+        if (data.accessToken) {
+          const decoded = jwtDecode(data.accessToken);
+
+          cookies.set('jwt-access-blog-p', data.accessToken, {
+              expires: new Date(decoded.exp * 1000),
+          });
+
+          accessToken = data.accessToken;
+        }
+      }
+
+      if (accessToken) {
+        const options = {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+        
+        fetch(url, options)
+          .then(res => res.json())
+          .then(data => {
+            if (data.errors)
+              throw new Error(data.errors[0].msg)
+            else if (data.posts)
+              setPosts(data.posts);
+          })
+      }
+    }
+
+    getPosts()
+  }, [])
 
   return (
       <Wrapper>
@@ -62,6 +110,23 @@ const Home = () => {
           {user && 
             <>
             <Header />
+            <main>
+                {posts && 
+                  <>
+                    {posts.length === 0 &&
+                        <h4>No posts found</h4>
+                    }
+                    {posts.length > 0 && posts.map((post) => (
+                        <div key={post.id}>
+                        <header className="post">
+                            <h2>{post.title}</h2>
+                            <div>{format(post.createdAt, "MMMM d, yyyy")}</div>
+                        </header>
+                        </div>
+                    ))}
+                  </>
+                }
+            </main>
             </>
           }
       </Wrapper>
